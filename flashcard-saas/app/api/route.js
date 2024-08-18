fa9e7bd85155e31bf0d63f28dd698726b0d7ffca
WebManDev <andyhluu23@gmail.com>
2024-08-17 23:49:55 -0400

    Update route.js

--- flashcard-saas/app/api/route.js
+++ flashcard-saas/app/api/route.js
@@ -47,3 +47,20 @@ return NextResponse.json(checkoutSession, {
     })
   }
 }
+export async function GET(req) {
+  const searchParams = req.nextUrl.searchParams
+  const session_id = searchParams.get('session_id')
+
+  try {
+    if (!session_id) {
+      throw new Error('Session ID is required')
+    }
+
+    const checkoutSession = await stripe.checkout.sessions.retrieve(session_id)
+
+    return NextResponse.json(checkoutSession)
+  } catch (error) {
+    console.error('Error retrieving checkout session:', error)
+    return NextResponse.json({ error: { message: error.message } }, { status: 500 })
+  }
+}
