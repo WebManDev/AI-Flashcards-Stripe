c243e89d540db8add7b6327f04265f5d9809645f
WebManDev <andyhluu23@gmail.com>
2024-08-17 23:49:17 -0400

    Update route.js

--- flashcard-saas/app/api/route.js
+++ flashcard-saas/app/api/route.js
@@ -17,7 +17,7 @@ export async function POST(req) {
         product_data: {
           name: 'Pro subscription',
         },
-        unit_amount: 1000, // $10.00 in cents
+        unit_amount: formatAmountForStripe(10, 'usd'), // $10.00
         recurring: {
           interval: 'month',
           interval_count: 1,
